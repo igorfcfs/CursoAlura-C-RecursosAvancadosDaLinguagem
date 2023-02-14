cdbf7dcd7abb8225807b7ce3ad5db3ddf5cb170d
igorfcfs <igorfcfs@gmail.com>
2023-02-14 18:37:57 -0300

    Recursividade

--- Programa/mapa.h
+++ Programa/mapa.h
@@ -1,8 +1,9 @@
 #define HEROI '@'
+#define FANTASMA 'F'
 #define VAZIO '.'
 #define PAREDE_VERTICAL '|'
 #define PAREDE_HORIZONTAL '-'
-#define FANTASMA 'F'
+#define PILULA 'P'
 
 struct mapa {
 	char** matriz;
@@ -12,11 +13,6 @@ struct mapa {
 
 typedef struct mapa MAPA;
 
-void alocamapa(MAPA* m);
-void lemapa(MAPA* m);
-void liberamapa(MAPA* m);
-void imprimemapa(MAPA* m);
-
 struct posicao {
 	int x;
 	int y;
@@ -24,15 +20,22 @@ struct posicao {
 
 typedef struct posicao POSICAO;
 
+void liberamapa(MAPA* m);
+void lemapa(MAPA* m);
+void alocamapa(MAPA* m);
+void imprimemapa(MAPA* m);
 int encontramapa(MAPA* m, POSICAO* p, char c);
 
 int ehvalida(MAPA* m, int x, int y);
-int ehparede(MAPA* m, int x, int y);
-int ehpersonagem(MAPA* m, char personagem, int x, int y);
-
+int ehvazia(MAPA* m, int x, int y);
 void andanomapa(MAPA* m, int xorigem, int yorigem,
 	int xdestino, int ydestino);
 
 void copiamapa(MAPA* destino, MAPA* origem);
 
 int podeandar(MAPA* m, char personagem, int x, int y);
+
+int ehpersonagem(MAPA* m, char personagem,
+	int x, int y);
+
+int ehparede(MAPA* m, int x, int y);
