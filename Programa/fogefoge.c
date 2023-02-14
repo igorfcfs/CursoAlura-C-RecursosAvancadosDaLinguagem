b596aba8d093f375b95f63c0f57db9d31cbdf475
igorfcfs <igorfcfs@gmail.com>
2023-02-14 18:47:25 -0300

    Diretivas

--- Programa/fogefoge.c
+++ Programa/fogefoge.c
@@ -3,10 +3,11 @@
 #include "time.h"
 #include "fogefoge.h"
 #include "mapa.h"
+#include "ui.h"
 
 MAPA m;
 POSICAO heroi;
-
+int tempilula = 0;
 
 int acabou() {
 	POSICAO pos;
@@ -28,9 +29,6 @@ int ehdirecao(char direcao) {
 
 void move(char direcao) {
 
-	if(!ehdirecao(direcao))
-		return;
-
 	int proximox = heroi.x;
 	int proximoy = heroi.y;
 
@@ -51,6 +49,10 @@ void move(char direcao) {
 
 	if(!podeandar(&m, HEROI, proximox, proximoy))
 		return;
+
+	if(ehpersonagem(&m, PILULA, proximox, proximoy)) {
+		tempilula=1;
+	}
 
 	andanomapa(&m, heroi.x, heroi.y, proximox, proximoy);
 	heroi.x = proximox;
@@ -105,18 +107,46 @@ void fantasmas() {
 	liberamapa(&copia);
 }
 
+void explodepilula2(int x, int y, int somax, int somay, int qtd) {
+
+	if(qtd == 0) return;
+
+	int novox = x+somax;
+	int novoy = y+somay;
+
+	if(!ehvalida(&m, novox, novoy)) return;
+	if(ehparede(&m, novox, novoy)) return;
+
+	m.matriz[novox][novoy] = VAZIO;
+	explodepilula2(novox, novoy, somax, somay, qtd-1);
+}
+
+void explodepilula() {
+	if(!tempilula) return;
+
+	explodepilula2(heroi.x, heroi.y, 0, 1, 3);
+	explodepilula2(heroi.x, heroi.y, 0, -1, 3);
+	explodepilula2(heroi.x, heroi.y, 1, 0, 3);
+	explodepilula2(heroi.x, heroi.y, -1, 0, 3);
+
+	tempilula = 0;
+}
+
 int main() {
 
 	lemapa(&m);
 	encontramapa(&m, &heroi, HEROI);
 
 	do {
+		printf("Pilula: %s\n", (tempilula ? "SIM" : "NAO"));
 		imprimemapa(&m);
 
 		char comando;
 		scanf(" %c", &comando);
 
-		move(comando);
+		if(ehdirecao(comando)) move(comando);
+		if(comando == BOMBA) explodepilula();
+
 		fantasmas();
 
 	} while (!acabou());
